dc3d8a6f872f2ab1d5cf580aace00643411dfae7
Venkata krishnan Sowrirajan <venkat.sowrirajan@gmail.com>
2026-05-25 10:04:34 -0700

    Update to Supervised pretraining deepdive and create launch_az.sh to launch with cute-chess

---
+++ launch_az.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Modify the below command accordingly to your env
+/export/apps/python/3.11/bin/python3.11 /Users/$USER/git/alphazero/scripts/uci_engine.py --checkpoint /Users/$USER/git/alphazero/pretrained_fixed.pt
